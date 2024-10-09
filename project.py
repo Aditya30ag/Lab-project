b70b0229ba2a8f75262ab8b727b8aa78b07d932d
Aditya30ag <adityaagrwal3005@gmail.com>
2024-10-09 17:23:55 +0530

    new commit

---
+++ project.py
@@ -0,0 +1,181 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import Image, ImageTk
+import os
+import threading
+import zipfile
+import gzip
+import shutil
+import py7zr
+
+def browse_input_file():
+    file_path = filedialog.askopenfilename()
+    input_file_entry.delete(0, tk.END)
+    input_file_entry.insert(0, file_path)
+
+def browse_output_file():
+    file_path = filedialog.asksaveasfilename()
+    output_file_entry.delete(0, tk.END)
+    output_file_entry.insert(0, file_path)
+
+
+#compressor
+def zipfilecompresser(input_file,output_file):
+    with zipfile.ZipFile(output_file + ".zip", 'w') as zipf:
+            # Add the file to the zip
+            zipf.write(input_file, os.path.basename(input_file))
+            print(f"{input_file} has been zipped into {output_file}.zip")
+            result_label.config(text=f"File '{input_file}' compressed to '{output_file}.zip'")
+
+def gzipfilecompressor(input_file,output_file):
+    with open(input_file, 'rb') as f_in:
+        with gzip.open(input_file+ ".gz", 'wb') as f_out:
+            f_out.writelines(f_in)
+    
+    result_label.config(text=f"File '{input_file}' compressed to '{output_file}.gz'")
+
+def py7zrfilecompressor(input_file,output_file):
+    with py7zr.SevenZipFile(output_file+".rar", mode='w') as archive:
+        archive.writeall(input_file, arcname='.')
+    result_label.config(text=f"File '{input_file}' compressed to '{output_file}.rar'")
+
+
+#Decompressor
+def zipfiledecompressor(input_file,output_file):
+    with zipfile.ZipFile(input_file, 'r') as zipf:
+        zipf.extractall(output_file)
+        result_label.config(text=f"File '{input_file}' decompressed to '{output_file}.zip'")
+
+def gzipfiledecompressor(input_file,output_file):
+    with open(input_file, 'rb') as f_in:
+        with gzip.open(output_file, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    
+    result_label.config(text=f"File '{input_file}' decompressed to '{output_file}'")
+
+def py7zrfiledecompressor(input_file,output_file):
+    with py7zr.SevenZipFile(input_file, 'r') as archive:
+        archive.extractall(path=output_file)
+    
+    result_label.config(text=f"File '{input_file}' decompressed to '{output_file}'")
+
+def compress_file():
+    input_file=input_file_entry.get()
+    output_file=output_file_entry.get()
+    format_choice=format_var.get()
+
+    try:
+        if not input_file or not output_file:
+            print("error")
+            raise ValueError("Please provide both input and output file paths.")
+        if not os.path.exists(input_file):
+            raise ValueError(f"Input File {input_file} not found")
+        
+        if format_choice=="GZIP":
+            output_file=input_file
+            gzipfilecompressor(input_file,output_file)
+        elif format_choice=="ZIP":
+            zipfilecompresser(input_file,output_file)
+        elif format_choice=="RAR":
+            py7zrfilecompressor(input_file,output_file)
+        else:
+            result_label.config(text="Invalid format selection.")
+    except Exception as e:
+        result_label.config(text=f"An error occurred: {e}")
+
+def compress_file_async():
+    threading.Thread(target=compress_file).start()
+
+def decompress_file():
+    input_file=input_file_entry.get()
+    output_file=output_file_entry.get()
+    format_choice=format_var.get()
+
+    try:
+        if not input_file or not output_file:
+            print("error")
+            raise ValueError("Please provide both input and output file paths.")
+        if not os.path.exists(input_file):
+            raise ValueError(f"Input File {input_file} not found")
+        
+        if format_choice=="GZIP":
+            gzipfiledecompressor(input_file,output_file)
+        elif format_choice=="ZIP":
+            zipfiledecompressor(input_file,output_file)
+        elif format_choice=="RAR":
+            py7zrfiledecompressor(input_file,output_file)
+        else:
+            result_label.config(text="Invalid format selection.")
+    except Exception as e:
+        result_label.config(text=f"An error occurred: {e}")
+
+def decompress_file_async():
+    threading.Thread(target=decompress_file).start()
+
+# Create the main application window
+app = tk.Tk()
+app.title("FileCompressor")
+app.minsize(800,480)
+app.maxsize(800,480)
+
+# Load the background image using Pillow
+background_image = Image.open("C:/Users/adity/Pictures/Screenshots/projectimg.png")  # Use your image path
+background_photo = ImageTk.PhotoImage(background_image)
+# Create a label for the background image
+background_label = tk.Label(app, image=background_photo)
+background_label.place(relwidth=1, relheight=1)  # Fill the entire window
+
+
+input_file_label = tk.Label(app, text="Input File:",background="#EDEAEF",padx=50,pady=10,font=("Arial",12,"bold"),relief="groove")
+input_file_label.pack()
+input_file_entry = tk.Entry(app,font=("Cursive",8),relief="flat",background="#312130",foreground="white")
+input_file_entry.pack()
+
+input_file_button = tk.Button(app, text="Browse",font=("Arial", 8),bg="#E8E8E8",fg="black",padx=4,pady=4,command=browse_input_file)
+input_file_button.pack()
+
+input_file_label.place(x=100, y=70)
+input_file_entry.place(x=358, y=80)
+input_file_button.place(x=576, y=78)
+
+output_file_label = tk.Label(app, text="Output File:",background="#EDEAEF",padx=45,pady=9,font=("Arial",12,"bold"),relief="groove")
+output_file_label.pack()
+output_file_entry = tk.Entry(app,background="#312130",foreground="white")
+output_file_entry.pack()
+output_file_button = tk.Button(app, text="Browse",font=("Arial", 8),bg="#E8E8E8",fg="black",padx=4,pady=4,command=browse_output_file)
+output_file_button.pack()
+
+output_file_label.place(x=100, y=170)
+output_file_entry.place(x=358, y=179)
+output_file_button.place(x=576, y=177)
+
+
+compress_button = tk.Button(app, text="Compress",padx=4,command=compress_file_async,background="#EDEAEF")
+compress_button.pack()
+
+decompress_button = tk.Button(app, text="Decompress",padx=3,background="#EDEAEF",command=decompress_file_async)
+decompress_button.pack()
+
+compress_button.place(x=280, y=370)
+decompress_button.place(x=480, y=370)
+
+
+format_var = tk.StringVar()
+format_var.set("GZIP")
+format_label = tk.Label(app, text="Select Compression/Decompression Format:")
+format_label.pack()
+format_menu = tk.OptionMenu(app, format_var, "GZIP", "ZIP", "RAR")
+format_menu.pack()
+
+format_label.place(x=300, y=320)
+format_menu.place(x=380, y=270)
+
+result_label = tk.Label(app, text="")
+result_label.pack(side="left")
+
+result_label.place(x=250,y=430)
+
+# Run the application
+app.mainloop()
+
